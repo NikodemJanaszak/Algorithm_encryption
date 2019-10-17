02bf9a936b46716287154cdd57df1f0a80a9b778
NikodemJanaszak <nikodem.janaszak@gmail.com>
2019-10-17 11:47:54 +0200

    First commit, window app. Builded few widnows

---
+++ src/sample/Controller.java
@@ -0,0 +1,19 @@
+package sample;
+
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+
+
+public class Controller {
+    public TextArea encryptOutput = new TextArea();
+    public TextArea encryptInput = new TextArea();
+    public void pressEncrypt(ActionEvent event){
+        System.out.println("Hello");
+
+        String text = encryptInput.getText();
+
+        encryptOutput.setText(text);
+    }
+
+}
