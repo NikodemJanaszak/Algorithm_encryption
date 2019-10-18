3c5c705eeccaf61006e88c6820edddc66fdf034b
NikodemJanaszak <nikodem.janaszak@gmail.com>
2019-10-18 18:12:21 +0200

    all encoding refactored and working workig

--- src/sample/CheckboardMap.java
+++ src/sample/CheckboardMap.java
@@ -10,56 +10,61 @@ public class CheckboardMap {
 
     public CheckboardMap(){
         this.encodeMap = new HashMap<Character, Integer>(){{
-            put('Q',0);put('W',1);put('E',2);
-            put('R',30);put('T',31);put('Y',32);put('U',33);put('I',34);put('O',35);put('P',36);put('A',37);put('S',38);put('D',39);
-            put('F',50);put('G',51);put('H',52);put('J',53);put('K',54);put('L',55);put('Z',56);put('X',57);put('C',58);put('V',59);
-            put('B',80);put('N',81);put('M',82);put('Ą',83);put('Ć',84);put('Ł',85);put('Ń',86);put('Ó',87);put('Ż',88);put('Ź',89);
-            put(',',40);put('.',41);put('q',42);put('w',43);put('e',44);put('r',45);put('t',46);put('y',47);put('u',48);put('i',49);
-            put('o',60);put('p',61);put('a',62);put('s',63);put('d',64);put('f',65);put('g',66);put('h',67);put('j',68);put('k',69);
-            put('l',90);put('z',91);put('x',92);put('c',93);put('v',94);put('b',95);put('n',96);put('m',97);put('ą',98);put('ć',99);
-            put('ł',70);put('ń',71);put('ó',72);put('ż',73);put('ź',74);put('ś',75);put('Ś',76);put(' ',77);
-        }};
+            put('q',0);put('w',1);put('e',3);put('t',6);put('y',7);put('u',8);put('i',9);
+            put('o',40);put('p',41);put('a',42);put('s',43);put('d',44);put('f',45);put('g',46);put('h',47);put('j',48);put('k',49);
+            put('l',20);put('z',21);put('x',22);put('c',23);put('v',24);put('b',25);put('n',26);put('m',27);put('ą',28);put('ć',29);
+            put('ę',50);put('ł',51);put('ń',52);put('ó',53);put('ś',54);put('ż',55);put('ź',56);put('.',57);put(',',58);put('r',59);
+            }};
 
         this.decodeMap = new HashMap<Integer, Character>(){{
-            put(0,'Q');put(1,'W');put(2,'E');
-            put(30,'R');put(31,'T');put(32,'Y');put(33,'U');put(34,'I');put(35,'O');put(36,'P');put(37,'A');put(38,'S');put(39,'D');
-            put(50,'F');put(51,'G');put(52,'H');put(53,'J');put(54,'K');put(55,'L');put(56,'Z');put(57,'X');put(58,'C');put(59,'V');
-            put(80,'B');put(81,'N');put(82,'M');put(83,'Ą');put(84,'Ć');put(85,'Ł');put(86,'Ń');put(87,'Ó');put(88,'Ż');put(89,'Ź');
-            put(40,',');put(41,'.');put(42,'q');put(43,'w');put(44,'e');put(45,'r');put(46,'t');put(47,'y');put(48,'u');put(49,'i');
-            put(60,'o');put(61,'p');put(62,'a');put(63,'s');put(64,'d');put(65,'f');put(66,'g');put(67,'h');put(68,'j');put(69,'k');
-            put(90,'l');put(91,'z');put(92,'x');put(93,'c');put(94,'v');put(95,'b');put(96,'n');put(97,'m');put(98,'ą');put(99,'ć');
-            put(70,'ł');put(71,'ń');put(72,'ó');put(73,'ż');put(74,'ź');put(75,'ś');put(76,'Ś');put(77,' ');
+            put(0,'q');put(1,'w');put(3,'e');put(6,'t');put(7,'y');put(8,'u');put(9,'i');
+            put(40,'o');put(41,'p');put(42,'a');put(43,'s');put(44,'d');put(45,'f');put(46,'g');put(47,'h');put(48,'j');put(49,'k');
+            put(20,'l');put(21,'z');put(22,'x');put(23,'c');put(24,'v');put(25,'b');put(26,'n');put(27,'m');put(28,'ą');put(29,'ć');
+            put(50,'ę');put(51,'ł');put(52,'ń');put(53,'ó');put(54,'ś');put(55,'ż');put(56,'ź');put(57,'.');put(58,',');put(59,'r');
         }};
     }
 
     public String encodeText(String toEncode){
         ArrayList<Integer> encoded = new ArrayList<>();
+        ArrayList<Integer> digits = new ArrayList<>();
         ArrayList<Integer> codeKey = new ArrayList<>(List.of(2,3));
         for (int i=0; i<toEncode.length(); i++){
             encoded.add(encodeMap.get(toEncode.charAt(i)));
         }
-        ArrayList<Integer>encodedV2 = new ArrayList<>();
 
-        for(int i=0; i<encoded.size();i++){
-            if(encoded.get(i)>9){
-                encodedV2.add(encoded.get(i)/10);
-                encodedV2.add(encoded.get(i)%10);
+        for (int i=0; i<encoded.size(); i++){
+            if(encoded.get(i)<10){
+                digits.add(encoded.get(i));
             }
             else {
-                encodedV2.add(encoded.get(i));
+                digits.add(encoded.get(i)/10);
+                digits.add(encoded.get(i)%10);
             }
         }
 
-        for (int i = 0; i < encodedV2.size(); i++) {
-            if(encodedV2.get(i)<5)
-                encodedV2.set(i, encodedV2.get(i) + codeKey.get(0));
-            else 
-                encodedV2.set(i, encodedV2.get(i) - codeKey.get(1));
-        }
-        encodedV2.add(codeKey.get(0));
-        encodedV2.add(codeKey.get(1));
 
-        return listIntToStrig(encodedV2);
+        Integer j=0;
+        for(int i=0; i<digits.size();i++){
+            if(digits.get(i)+codeKey.get(j)>10){
+                int temp = (digits.get(i)+codeKey.get(j))%10;
+                digits.set(i, temp);
+            }
+            else
+                digits.set(i, digits.get(i) + codeKey.get(j));
+            if(j==1)
+                j=0;
+            else
+                j++;
+        }
+
+        for (Integer i: digits){
+            System.out.println(i);
+        }
+
+        digits.add(codeKey.get(0));
+        digits.add(codeKey.get(1));
+
+        return listIntToStrig(digits);
     }
 
     public String listIntToStrig(ArrayList<Integer> intList){
@@ -74,24 +79,27 @@ public class CheckboardMap {
         ArrayList<Character> decoded = new ArrayList<>();
         ArrayList<Integer> toDecodeInt = new ArrayList<>();
 
-        for (int i=0; i<toDecode.length();i++){
+        for (int i=0; i<toDecode.length()-2;i+=2){
             int temp = toDecode.charAt(i)-'0';
-            toDecodeInt.add(temp);
+            int temp2 = toDecode.charAt(i+1)-'0';
+            int toArray = temp*10+temp2;
+            toDecodeInt.add(toArray);
         }
-        Integer lastKey = toDecodeInt.get(toDecodeInt.size()-1);
-        Integer midKey = toDecodeInt.get(toDecodeInt.size()-2);
-        Integer firstKey = toDecodeInt.get(toDecodeInt.size()-3);
+        Integer lastKey = toDecode.charAt(toDecode.length()-1)-'0';
+        Integer firstKey = toDecode.charAt(toDecode.length()-2)-'0';
 
-        ArrayList<Integer> codeKey = new ArrayList<>(List.of(firstKey,midKey,lastKey));
+        for(Integer i : toDecodeInt){
+            System.out.println(i);
+        }
+
+        ArrayList<Integer> codeKey = new ArrayList<>(List.of(firstKey,lastKey));
 
         int j=0;
-        for (int i=0; i<toDecodeInt.size()-3;i++){
+        for (int i=0; i<toDecodeInt.size()-2;i++){
             System.out.println(toDecodeInt.get(i));
+            //if(toDecodeInt.get(i)-)
+
             toDecodeInt.set(i,toDecodeInt.get(i)/codeKey.get(j));
-            if(j==2)
-                j=0;
-            else
-                j++;
             System.out.println(toDecodeInt.get(i));
         }
 
